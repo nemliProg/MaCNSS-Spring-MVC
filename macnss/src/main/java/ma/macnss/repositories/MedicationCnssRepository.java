0124adb0317d11421e01facf2f11693c3bd6a092
nemliProg <younesbuzz@gmail.com>
2022-12-11 01:15:25 +0100

    create Medication Cnss Repository to get static data

---
+++ macnss/src/main/java/ma/macnss/repositories/MedicationCnssRepository.java
@@ -0,0 +1,7 @@
+package ma.macnss.repositories;
+
+import ma.macnss.entities.MedicamentsCnss;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicationCnssRepository extends JpaRepository<MedicamentsCnss,String> {
+}
