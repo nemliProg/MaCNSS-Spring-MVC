df275acfcb6f7522795ad16675641f5dc09dd5c7
nemliProg <younesbuzz@gmail.com>
2022-12-12 13:35:03 +0100

    add method login to verify logged Client from database

--- macnss/src/main/java/ma/macnss/dao/ClientDao.java
+++ macnss/src/main/java/ma/macnss/dao/ClientDao.java
@@ -1,7 +1,9 @@
 package ma.macnss.dao;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import ma.macnss.entities.Agent;
 import ma.macnss.entities.Client;
 import ma.macnss.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @NoArgsConstructor
@@ -19,4 +22,16 @@ public class ClientDao {
     @Autowired
     private ClientRepository clientRepository;
 
+    public boolean login(String matricule, String password, HttpServletRequest request){
+        Optional<Client> client = clientRepository.findById(matricule);
+        if(client.isPresent()){
+            if(client.get().getPassword().equals(password)){
+                request.getSession().setAttribute("id",client.get().getMatricule());
+                request.getSession().setAttribute("client",client.get());
+                System.out.println("Client logged in");
+                return true;
+            }
+        }
+        return false;
+    }
 }
