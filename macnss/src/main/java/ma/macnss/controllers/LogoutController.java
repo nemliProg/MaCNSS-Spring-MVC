a8bcd99633a9eb07fddc741dd70241f7e146452f
nemliProg <younesbuzz@gmail.com>
2022-12-13 14:24:58 +0100

    add Logout controller to logout all users

---
+++ macnss/src/main/java/ma/macnss/controllers/LogoutController.java
@@ -0,0 +1,16 @@
+package ma.macnss.controllers;
+
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LogoutController {
+
+        @PostMapping("/logout")
+        public String logout(HttpSession session){
+            session.invalidate();
+            return "redirect:/";
+        }
+}
